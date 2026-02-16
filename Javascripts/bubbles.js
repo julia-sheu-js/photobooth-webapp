388348cc8ad3486e388e815dd7e7f49bc0a04e17
Julia <jsheu2021@gmail.com>
2026-02-16 22:05:44 +0800

    Fix bubble paths and use 5 unique bubbles

--- Javascripts/bubbles.js
+++ Javascripts/bubbles.js
@@ -1,15 +1,13 @@
 // connect to bubble container
 const bubbleContainer = document.querySelector(".bubble-container");
 
-// get images
+// get images - 5 unique bubbles
 const bubbleImages = [
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble4.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble1.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble2.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble3.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble4.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble5.png",
-  "../Assets/fish-photobooth/camerapage/bubbles/bubble4.png"
+  "Assets/fish-photobooth/camerapage/bubbles/bubble1.png",
+  "Assets/fish-photobooth/camerapage/bubbles/bubble2.png",
+  "Assets/fish-photobooth/camerapage/bubbles/bubble3.png",
+  "Assets/fish-photobooth/camerapage/bubbles/bubble4.png",
+  "Assets/fish-photobooth/camerapage/bubbles/bubble5.png"
 ];
 
 // create bubble
@@ -17,19 +15,16 @@ const createBubble = () => {
   const bubble = document.createElement("img");
   bubble.src = bubbleImages[Math.floor(Math.random() * bubbleImages.length)];
   bubble.classList.add("bubble");
-
+  
   // random position, size, duration
   bubble.style.left = Math.random() * 100 + "vw";
   const size = 20 + Math.random() * 20;
   bubble.style.width = size + "px";
   const duration = 12 + Math.random() * 8;
   bubble.style.animationDuration = duration + "s";
-
-  // random final opacity
-  bubble.addEventListener("animationend", () => bubble.style.opacity = 0.2 + Math.random() * 0.8);
-
+  
   bubbleContainer.appendChild(bubble);
-
+  
   // remove after animation
   setTimeout(() => bubble.remove(), duration * 1000);
 };
